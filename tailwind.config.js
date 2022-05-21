76be4a61bafafb0ae512e362ac7f0ceda72c2490
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-22 00:51:34 +0600

    explore auto-parts online-store assignment-12

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#FF3737',
+
+          secondary: '#666666',
+
+          accent: '#37CDBE',
+
+          neutral: '#3D4451',
+
+          'base-100': '#FFFFFF',
+
+          info: '#3ABFF8',
+
+          success: '#36D399',
+
+          warning: '#FBBD23',
+
+          error: '#ff0000',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+};
