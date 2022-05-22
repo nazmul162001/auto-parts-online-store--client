7c34dcbe60a436ac30fa66173d361cd907b5e799
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-22 11:11:00 +0600

    responsive banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,19 +5,19 @@ import './Banner.css';
 
 const Banner = () => {
   return (
-    <section className='banner w-full h-screen bg-black grid grid-cols-1 md:grid-cols-2 justify-between items-center overflow-x-hidden'>
-      <div className="banner-content-main grid grid-cols-2 items-center">
-        <div className="banner-content pl-12">
+    <section className='banner w-full h-screen overflow-hidden bg-black grid grid-cols-1 md:grid-cols-2 justify-between items-center overflow-x-hidden'>
+      <div className="banner-content-main grid grid-cols-1 md:grid-cols-2 items-center justify-center">
+        <div className="banner-content pl-12 text-center">
         <span className='text-white bg-primary rounded-full px-3 py-1'>Buy In Auto Parts</span>
         <h1 className="text-4xl text-white banner-title py-3">Auto Parts Online Store</h1>
         <p className='text-white banner-subTitle pb-5'>Special offer for auto parts for electric cars and hybrid vehicles. To select the spare part you need, use the filter presented above or the advanced filter.</p>
-        <button className='btn text-black bg-white px-14 rounded-sm'>Get Start</button>
+        <button className='btn text-black bg-white px-14 rounded-sm hover:text-white'>Get Start</button>
         </div>
-        <div className="banner-bg">
+        <div className="banner-bg hidden md:block">
           <img className='h-full w-full ml-[100px]' src={bg} alt="" />
         </div>
       </div>
-      <div className="banner-image">
+      <div className="banner-image mt-14 md:mt-0">
         <img src={car} alt="" />
       </div>
     </section>
--- src/Pages/Shared/NavbarMini.js
+++ src/Pages/Shared/NavbarMini.js
@@ -3,7 +3,7 @@ import { FiPhoneCall } from 'react-icons/fi';
 
 const NavbarMini = () => {
   return (
-    <div className='hidden lg:block'>
+    <div className='hidden lg:block static z-10'>
       <div className="second-navbar h-10 my-3 text-white px-12 flex justify-between items-center">
         <div className='flex items-center'>
           <ul className="menu menu-horizontal p-0 btn btn-black rounded-none bg-black mr-8">
