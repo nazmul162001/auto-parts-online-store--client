d48c78774fbbe326b6c2a97e96029a4eca9cd7f3
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-24 12:37:10 +0600

    verify jwt & signOut user without token

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,17 +1,34 @@
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const MyOrders = () => {
   const [orders, setOrders] = useState([]);
-
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
-      fetch(`http://localhost:5000/order?email=${user.email}`)
-        .then((res) => res.json())
-        .then((data) => setOrders(data));
+      fetch(`http://localhost:5000/order?email=${user.email}`, {
+        method: 'GET',
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        },
+      })
+        .then((res) => {
+          // console.log('res', res);
+          if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem('accessToken');
+            navigate('/');
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setOrders(data);
+        });
     }
   }, [user]);
 
