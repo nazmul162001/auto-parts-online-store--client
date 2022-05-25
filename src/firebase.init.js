1d8b3573aed7bd14529f5d7b2f84fa8d8bdb541f
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-26 02:07:11 +0600

    secure firebase init file

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,13 +1,13 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzHKvJvjBRRgaBetQT2jtNn-tzhT7AaYA",
-  authDomain: "auto-parts-store-6bc80.firebaseapp.com",
-  projectId: "auto-parts-store-6bc80",
-  storageBucket: "auto-parts-store-6bc80.appspot.com",
-  messagingSenderId: "749152675610",
-  appId: "1:749152675610:web:6dffa58bdb605f5ceceea3",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
