2453f114c6ff6572c58b3d9d5964868a21cf02c9
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-25 13:51:53 +0600

    added portfolio image & set card style

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import warehouse from '../../assets/images/portfolio/warehouse.png';
+import fitness from '../../assets/images/portfolio/fitness-corner.png';
+import analysis from '../../assets/images/portfolio/product-analysis.png';
+import todos from '../../assets/images/portfolio/todos.png';
 
 const Portfolio = () => {
   return (
@@ -71,17 +75,14 @@ const Portfolio = () => {
         <h2 className="text-center text-5xl text-gray-500 font-bold my-5">
           Recent Project
         </h2>
-        <div className="project grid grid-cols-1 md:grid-cols-3">
-          <div class="card w-96 bg-base-100 shadow-xl">
+        <div className="project grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div class="card w-full bg-base-100 card-shadow">
             <figure>
-              <img
-                src="https://api.lorem.space/image/shoes?w=400&h=225"
-                alt="Shoes"
-              />
+              <img src={warehouse} alt="" />
             </figure>
             <div class="card-body">
               <h2 class="card-title">
-                Shoes!
+                Warehouse Management (Fruits)
                 <div class="badge badge-secondary">NEW</div>
               </h2>
               <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -91,16 +92,13 @@ const Portfolio = () => {
               </div>
             </div>
           </div>
-          <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card w-full bg-base-100 card-shadow">
             <figure>
-              <img
-                src="https://api.lorem.space/image/shoes?w=400&h=225"
-                alt="Shoes"
-              />
+              <img src={fitness} alt="" />
             </figure>
             <div class="card-body">
               <h2 class="card-title">
-                Shoes!
+                Fitness Corner (Gym)
                 <div class="badge badge-secondary">NEW</div>
               </h2>
               <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -110,16 +108,29 @@ const Portfolio = () => {
               </div>
             </div>
           </div>
-          <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card w-full bg-base-100 card-shadow">
             <figure>
-              <img
-                src="https://api.lorem.space/image/shoes?w=400&h=225"
-                alt="Shoes"
-              />
+              <img src={analysis} alt="" />
             </figure>
             <div class="card-body">
               <h2 class="card-title">
-                Shoes!
+                Product Analysis (Headphone)
+                <div class="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+                <div class="badge badge-outline">Fashion</div>
+                <div class="badge badge-outline">Products</div>
+              </div>
+            </div>
+          </div>
+          <div class="card w-full bg-base-100 card-shadow">
+            <figure>
+              <img src={todos} alt="" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">
+                Todo App
                 <div class="badge badge-secondary">NEW</div>
               </h2>
               <p>If a dog chews shoes whose shoes does he choose?</p>
