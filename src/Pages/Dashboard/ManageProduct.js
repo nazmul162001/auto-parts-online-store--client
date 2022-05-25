285e8be2c9f3ca1e361036780f4d9703514bab21
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-26 01:46:48 +0600

    update code

--- src/Pages/Dashboard/ManageProduct.js
+++ src/Pages/Dashboard/ManageProduct.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
+import Spinner from '../Shared/Spinner';
+import ManageProductInfo from './ManageProductInfo';
 
 const ManageProduct = () => {
+  // useQuery
+  const {
+    data: services,
+    isloading,
+    refetch,
+  } = useQuery('item', () =>
+    fetch('http://localhost:5000/service').then((res) => res.json())
+  );
+  if (isloading) {
+    return <Spinner />;
+  }
+
   return (
-    <div>
-      <h2 className="text-3xl text-gray-500">Manage Product</h2>
+    <div className="my-16 px-12">
+      <div className="service-title text-center">
+        <h3 className="text-primary text-5xl font-bold opacity-60">
+          Manage Your Product
+        </h3>
+        <div className="line w-96 opacity-60 my-4 h-[2px] bg-primary mx-auto"></div>
+      </div>
+
+      {/* available parts  */}
+      <div className="available-parts grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {services?.map((service) => (
+          <ManageProductInfo key={service._id} service={service} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Dashboard/ManageProductInfo.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { MdKeyboardArrowRight } from 'react-icons/md';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { BsSuitHeart } from 'react-icons/bs';
+import { Link, useParams } from 'react-router-dom';
+
+const ManageProductInfo = ({ service }) => {
+  const [color, setColor] = useState(false);
+  const {
+    img,
+    name,
+    description,
+    minimumOrder,
+    availableQuantity,
+    price,
+    _id,
+  } = service;
+
+  // const handleDelete = () => {
+  //   const url = `http://localhost:5000/service/${_id}`;
+  //   fetch(url, {
+  //     method: 'DELETE',
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       if (data.deletedCound > 0) {
+  //         const remaining = service.filter((order) => order._id !== id);
+  //         setOrders(remaining);
+  //       }
+  //     });
+  // };
+
+  return (
+    <div className="available-part border-2 rounded-md">
+      <div className="card w-96 h-[650px] bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img className="w-56 p-8 rounded-xl" src={img} alt="/" />
+        </figure>
+        <div className="card-body text-justify">
+          <h2 className="card-title">{name} </h2>
+          <p>{description.slice(0, 150)}</p>
+          <div className="order-minimum flex justify-between items-center">
+            <p className="flex items-center ">
+              <MdKeyboardArrowRight className="text-primary text-xl" /> Minimum
+              Order
+            </p>
+            <p className="text-right">{minimumOrder}</p>
+          </div>
+          <div className="quantity flex justify-between items-center">
+            <p className="flex items-center ">
+              <MdKeyboardArrowRight className="text-primary text-xl" />{' '}
+              Available Quantity
+            </p>
+            <p className="text-right">{availableQuantity}</p>
+          </div>
+          <div className="price flex justify-between items-center">
+            <p className="flex items-center font-bold text-xl">
+              {' '}
+              <BsCurrencyDollar className="" /> {price}
+            </p>
+            <div>
+              <p
+                onClick={() => {
+                  setColor(!color);
+                }}
+                className="text-right cursor-pointer"
+              >
+                <BsSuitHeart
+                  className={
+                    color ? 'text-gray-500 text-2xl' : 'text-primary text-2xl'
+                  }
+                />{' '}
+              </p>
+            </div>
+          </div>
+          {/* <button className="btn btn-outline">Purchase</button> */}
+          <div className="purchase-btn">
+            <button
+              // onClick={handleDelete}
+              className="btn-style w-full bg-red-500 mt-3"
+            >
+              <span></span>
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ManageProductInfo;
