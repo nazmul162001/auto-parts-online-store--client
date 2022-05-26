bb9c768c01206096ea352d9e2eb45eded6998697
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-26 23:49:15 +0600

    added how will you improve performance in react

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -9,7 +9,6 @@ const Blogs = () => {
       </h2>
       <div className="accordion px-8 mb-12">
         <div id="accordion-collapse" data-accordion="collapse">
-
           <h2 id="accordion-collapse-heading-1">
             <button
               type="button"
@@ -18,7 +17,10 @@ const Blogs = () => {
               aria-expanded="true"
               aria-controls="accordion-collapse-body-1"
             >
-              <span><strong>Q1. </strong> How will you improve the performance of a react applications?</span>
+              <span>
+                <strong>Q1. </strong> How will you improve the performance of a
+                react applications?
+              </span>
             </button>
           </h2>
           <div
@@ -28,20 +30,34 @@ const Blogs = () => {
           >
             <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
               <p class="mb-2 text-gray-500 dark:text-gray-400">
-               Flowbite is an open-source library of interactive components
-                built on top of Tailwind CSS including buttons, dropdowns,
-                modals, navbars, and more.
+                To Improve our react applications performance Keeping component
+                state local where necessary We have learned that a state update
+                in a parent component re-renders the parent and its child
+                components.So, to ensure re-rendering a component only happens
+                when necessary, we can extract the part of code that cares about
+                the component state, making it local to that part of the code.
               </p>
-              <p class="text-gray-500 dark:text-gray-400">
-                Check out this guide to learn how to{' '}
-                <a
-                  href="https://flowbite.com/docs/getting-started/introduction/"
-                  class="text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  get started
-                </a>{' '}
-                and start developing websites even faster with components on top
-                of Tailwind CSS.
+              <p className="my-1">
+                {' '}
+                (i) Using the useCallback Hook With the useCallback Hook, the
+                incrementCount function only redefines when the count dependency
+                array changes
+              </p>
+              <p className="my-1">
+                {' '}
+                (ii) Using the useMemo Hook When the prop we pass down to a
+                child component is an array or object, we can use a useMemo Hook
+                to memoize the value between renders. As we have learned above,
+                these values point to different spaces in memory and are
+                entirely new values.
+              </p>
+              <p className="my-1">
+                {' '}
+                (iii) Lazy loading images in React To optimize an application
+                that consists of several images, we can avoid rendering all of
+                the images at once to improve the page load time. With lazy
+                loading, we can wait until each of the images is about to appear
+                in the viewport before we render them in the DOM.
               </p>
             </div>
           </div>
@@ -53,42 +69,10 @@ const Blogs = () => {
               aria-expanded="true"
               aria-controls="accordion-collapse-body-1"
             >
-              <span><strong>Q2. </strong> What are the different way to manage a state in a different way?</span>
-            </button>
-          </h2>
-          <div
-            id="accordion-collapse-body-1"
-            class=""
-            aria-labelledby="accordion-collapse-heading-1"
-          >
-            <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-              <p class="mb-2 text-gray-500 dark:text-gray-400">
-                Flowbite is an open-source library of interactive components
-                built on top of Tailwind CSS including buttons, dropdowns,
-                modals, navbars, and more.
-              </p>
-              <p class="text-gray-500 dark:text-gray-400">
-                Check out this guide to learn how to{' '}
-                <a
-                  href="https://flowbite.com/docs/getting-started/introduction/"
-                  class="text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  get started
-                </a>{' '}
-                and start developing websites even faster with components on top
-                of Tailwind CSS.
-              </p>
-            </div>
-          </div>
-          <h2 id="accordion-collapse-heading-1">
-            <button
-              type="button"
-              class="flex justify-between items-center p-5 w-full font-medium text-left rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white"
-              data-accordion-target="#accordion-collapse-body-1"
-              aria-expanded="true"
-              aria-controls="accordion-collapse-body-1"
-            >
-              <span><strong>Q3. </strong> How does prototypical inheritance work?</span>
+              <span>
+                <strong>Q2. </strong> What are the different way to manage a
+                state in a different way?
+              </span>
             </button>
           </h2>
           <div
@@ -123,7 +107,9 @@ const Blogs = () => {
               aria-expanded="true"
               aria-controls="accordion-collapse-body-1"
             >
-              <span><strong>Q4. </strong> Why You do not set the state directly in react?</span>
+              <span>
+                <strong>Q3. </strong> How does prototypical inheritance work?
+              </span>
             </button>
           </h2>
           <div
@@ -158,7 +144,10 @@ const Blogs = () => {
               aria-expanded="true"
               aria-controls="accordion-collapse-body-1"
             >
-              <span><strong>Q5. </strong> You have an array of product. Each object has a name, price, description etc. How will you implement a search to find product by name?</span>
+              <span>
+                <strong>Q4. </strong> Why You do not set the state directly in
+                react?
+              </span>
             </button>
           </h2>
           <div
@@ -193,7 +182,11 @@ const Blogs = () => {
               aria-expanded="true"
               aria-controls="accordion-collapse-body-1"
             >
-              <span><strong>Q6. </strong> What is a unit test? Why should write unit tests?</span>
+              <span>
+                <strong>Q5. </strong> You have an array of product. Each object
+                has a name, price, description etc. How will you implement a
+                search to find product by name?
+              </span>
             </button>
           </h2>
           <div
@@ -220,7 +213,44 @@ const Blogs = () => {
               </p>
             </div>
           </div>
-
+          <h2 id="accordion-collapse-heading-1">
+            <button
+              type="button"
+              class="flex justify-between items-center p-5 w-full font-medium text-left rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white"
+              data-accordion-target="#accordion-collapse-body-1"
+              aria-expanded="true"
+              aria-controls="accordion-collapse-body-1"
+            >
+              <span>
+                <strong>Q6. </strong> What is a unit test? Why should write unit
+                tests?
+              </span>
+            </button>
+          </h2>
+          <div
+            id="accordion-collapse-body-1"
+            class=""
+            aria-labelledby="accordion-collapse-heading-1"
+          >
+            <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+              <p class="mb-2 text-gray-500 dark:text-gray-400">
+                Flowbite is an open-source library of interactive components
+                built on top of Tailwind CSS including buttons, dropdowns,
+                modals, navbars, and more.
+              </p>
+              <p class="text-gray-500 dark:text-gray-400">
+                Check out this guide to learn how to{' '}
+                <a
+                  href="https://flowbite.com/docs/getting-started/introduction/"
+                  class="text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  get started
+                </a>{' '}
+                and start developing websites even faster with components on top
+                of Tailwind CSS.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
